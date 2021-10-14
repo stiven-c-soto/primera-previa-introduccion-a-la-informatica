4f3408516eacbe9e2a8deb1f8ae8184a6b54219c
stiven-c-soto <89593930+stiven-c-soto@users.noreply.github.com>
2021-10-14 09:53:40 -0500

    cuarto trabajo

---
+++ funciones.js
@@ -0,0 +1,104 @@
+document.write("NÚMEROS BINARIOS<br>");
+document.write("-----------------------------<br><br>");
+document.write("Número: 0000 = ");
+
+var n;
+//1
+n = 0 * (Math.pow(2,3)) + 0 * (Math.pow(2,2)) + 0 * (Math.pow(2,1)) + 0 * (Math.pow(2,0));
+
+document.write(n + "<br>");
+
+document.write("Número: 0001 = ");
+// 2
+var n;
+
+n = 0 * (Math.pow(2,3)) + 0 * (Math.pow(2,2)) + 0 * (Math.pow(2,1)) + 1 * (Math.pow(2,0));
+
+document.write(n + "<br>");
+//3
+document.write("Numero: 0010 = ");
+n = 0 * (Math.pow(2,3)) + 0 * (Math.pow(2,2)) + 1 * (Math.pow(2,1)) + 0 * (Math.pow(2,0));
+
+document.write(n + "<br>");
+//4
+document.write("Numero: 0011 = ");
+n = 0 * (Math.pow(2,3)) + 0 * (Math.pow(2,2)) +  1 * (Math.pow(2,1)) + 1 * (Math.pow(2,0));
+
+//5
+document.write(n + "<br>");
+
+document.write("Numero: 0100 = ");
+n = 0 * (Math.pow(2,3)) + 1 * (Math.pow(2,2)) + 0 * (Math.pow(2,1)) + 0 * (Math.pow(2,0));
+document.write(n + "<br>");
+
+//6
+document.write("Numero: 0101 = ");
+n = 0 * (Math.pow(2,3)) + 1 * (Math.pow(2,2)) +  0 * (Math.pow(2,1)) + 1 * (Math.pow(2,0));
+
+document.write(n + "<br>");
+
+//7
+document.write("Numero: 0110 = ");
+n = 0 * (Math.pow(2,3)) + 1 * (Math.pow(2,2)) +  1 * (Math.pow(2,1)) + 0 * (Math.pow(2,0));
+
+document.write(n + "<br>");
+
+//8
+document.write("Numero: 0111 = ");
+n = 0 * (Math.pow(2,3)) + 1 * (Math.pow(2,2)) +  1 * (Math.pow(2,1)) + 1 * (Math.pow(2,0));
+
+document.write(n + "<br>");
+
+//9
+document.write("Numero: 1000 = ");
+n = 1 * (Math.pow(2,3)) + 0 * (Math.pow(2,2)) + 0 * (Math.pow(2,1)) + 0 * (Math.pow(2,0));
+document.write(n + "<br>");
+
+//10
+document.write("Numero: 1001 = ");
+n = 1 * (Math.pow(2,3)) + 0 * (Math.pow(2,2)) +  0 * (Math.pow(2,1)) + 1 * (Math.pow(2,0));
+
+document.write(n + "<br>");
+
+//11
+document.write("Numero: 1010 = ");
+n = 1 * (Math.pow(2,3)) + 0 * (Math.pow(2,2)) +  1 * (Math.pow(2,1)) + 0 * (Math.pow(2,0));
+
+document.write(n + "<br>");
+
+//12
+document.write("Numero: 1011 = ");
+n = 1 * (Math.pow(2,3)) + 0 * (Math.pow(2,2)) +  1 * (Math.pow(2,1)) + 1 * (Math.pow(2,0));
+
+document.write(n + "<br>");
+
+//13
+document.write("Numero: 1100 = ");
+n = 1 * (Math.pow(2,3)) + 1 * (Math.pow(2,2)) +  0 * (Math.pow(2,1)) + 0 * (Math.pow(2,0));
+
+document.write(n + "<br>");
+
+//14
+document.write("Numero: 1101 = ");
+n = 1 * (Math.pow(2,3)) + 1 * (Math.pow(2,2)) +  0 * (Math.pow(2,1)) + 1 * (Math.pow(2,0));
+
+document.write(n + "<br>");
+
+//15
+document.write("Numero: 1110 = ");
+n = 1 * (Math.pow(2,3)) + 1 * (Math.pow(2,2)) +  1 * (Math.pow(2,1)) + 0 * (Math.pow(2,0));
+
+document.write(n + "<br>");
+
+//16
+document.write("Numero: 1111 = ");
+n = 1 * (Math.pow(2,3)) + 1 * (Math.pow(2,2)) +  1 * (Math.pow(2,1)) + 1 * (Math.pow(2,0));
+
+document.write(n + "<br>");
+
+
+
+
+
+
+
